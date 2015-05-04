e199e8b9a4ecc91aab34a06b47d56b9e00cb8cdc
MaxSZP <MaxSol.ZP@gmail.com>
2015-05-04 03:28:22 +0300

    Master
    Test

---
+++ makszp-pp-ua/index.php
@@ -0,0 +1,23 @@
+<html>
+<DOCTYPE HTML>
+
+
+<head>
+
+</head>
+
+<body>
+
+
+Вот книга о Гите:
+git-scm.com/book/ru/v1
+
+
+А данная статья как раз о том как начать пользоваться GitHub:
+habrahabr.ru/post/125799
+
+
+
+</body>
+
+</html>
